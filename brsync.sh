94cb409f4268aa596ac335bf7a3efb15949171c6
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-02-22 03:22:42 +0100

    use /usr/bin/env

--- brsync.sh
+++ brsync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 # Creates local tracking branches for remote branches not merged
--- git-rm-branches.sh
+++ git-rm-branches.sh
@@ -1,3 +1,3 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 git branch | grep -v main | grep -v '*' | xargs git branch -D
