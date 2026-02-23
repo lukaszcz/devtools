92f1dad33aa3deeb60a19a95a379cc1c674f28e2
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-02-23 10:06:10 +0100

    setup-git-aliases.sh

--- setup-git-aliases.sh
+++ setup-git-aliases.sh
@@ -5,5 +5,5 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.wt worktree
-git config --global alias.lg "log --oneline -5"
+git config --global alias.lg "log --oneline -10"
 git config --global alias.lgs "log --oneline"
