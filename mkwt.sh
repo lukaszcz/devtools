bbf384441db8fa72ee69e775bbc0a79c2dccce3c
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-03-06 17:58:25 +0100

    mkwt.sh

--- mkwt.sh
+++ mkwt.sh
@@ -34,7 +34,7 @@ if $CREATE_BRANCH; then
 else
     git worktree add "$DIRNAME" "$BRANCH"
 fi
-cp -r .setup.sh .env .env.local .config .opencode .codex .claude .mcp.json $DIRNAME 2>/dev/null || true
+cp -r .setup.sh .env .env.local .config .agents .opencode .codex .claude .mcp.json $DIRNAME 2>/dev/null || true
 
 cd $DIRNAME
 
