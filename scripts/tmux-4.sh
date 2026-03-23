32aa08aba18e697ba90816aebdf65ac032636bca
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-03-23 10:27:54 +0100

    tmux env

--- scripts/tmux-4.sh
+++ scripts/tmux-4.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 session_args=()
 
@@ -6,7 +6,61 @@ if [[ -n "${1:-}" ]]; then
   session_args=(-s "$1")
 fi
 
-tmux new-session "${session_args[@]}" \; \
+emulate -L zsh
+setopt extendedglob
+
+# Exact variable names to skip.
+local -a skip_names=(
+  TMUX
+  TERM
+  DISPLAY
+  PWD
+  OLDPWD
+  SHELL
+  SHLVL
+  LOGNAME
+  USER
+  _
+)
+
+# Variable-name prefixes to skip.
+local -a skip_prefixes=(
+  TMUX_
+  TERM_
+  SSH_
+  DBUS_
+  XDG_
+)
+
+local -a tmux_env_args
+local line name value prefix
+local skip
+
+while IFS= read -r line; do
+  name=${line%%=*}
+  value=${line#*=}
+
+  skip=0
+
+  # Skip exact names.
+  if (( ${skip_names[(Ie)$name]} )); then
+    skip=1
+  else
+    # Skip matching prefixes.
+    for prefix in "${skip_prefixes[@]}"; do
+      if [[ $name == ${prefix}* ]]; then
+        skip=1
+        break
+      fi
+    done
+  fi
+
+  (( skip )) && continue
+
+  tmux_env_args+=(-e "$name=$value")
+done < <(env)
+
+tmux new-session "${session_args[@]}" "${tmux_env_args[@]}" \; \
   split-window -h \; \
   split-window -h \; \
   split-window -h \; \
